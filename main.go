3ad5a200bcd414da25785ad66ecfd2eb35fd0976
Josh Alexakos <josiah.alexakos@gmail.com>
2026-01-27 12:50:06 -0500

    Chapter 1, lesson 5

--- main.go
+++ main.go
@@ -6,6 +6,25 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(map[string]cliCommand) error
+}
+
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -16,6 +35,24 @@ func main() {
 		}
 		inputText := scanner.Text()
 		firstWord := cleanInput(inputText)[0]
-		fmt.Printf("Your command was: %v\n", firstWord)
+		if command, exists := commands[firstWord]; exists {
+			command.callback(commands)
+		} else {
+			fmt.Println("Unknown command. Type 'help' for available commands.")
+		}
 	}
+}
+
+func commandExit(cmds map[string]cliCommand) error {
+	fmt.Print("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp(cmds map[string]cliCommand) error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, info := range cmds {
+		fmt.Printf("%s: %s\n", info.name, info.description)
+	}
+	return nil
 }
